ff335c4f962a5e8bb85f5b091b99a371bf1c9404
liblaf <30631553+liblaf@users.noreply.github.com>
2025-03-07 15:55:09 +0800

    refactor(notify): move GetLabel function to cloudflare package
    - Relocates GetLabel function from notify/format.go to cloudflare package for better code organization and reusability
    - Updates PrettyRecord function to use the relocated GetLabel function

--- pkg/notify/format.go
+++ pkg/notify/format.go
@@ -4,21 +4,15 @@ import (
 	"fmt"
 
 	"github.com/cloudflare/cloudflare-go/v4/dns"
+	"github.com/liblaf/ddns/pkg/cloudflare"
 )
-
-func GetLabel(record dns.RecordResponse) string {
-	if record.Comment != "" {
-		return record.Comment
-	}
-	return record.Name
-}
 
 func PrettyRecord(emoji string, record dns.RecordResponse) string {
 	return fmt.Sprintf(
 		"%s <a href=\"%s\">%s</a> => <code>%s</code>",
 		emoji,
 		record.Name,
-		GetLabel(record),
+		cloudflare.GetLabel(record),
 		record.Content,
 	)
 }
