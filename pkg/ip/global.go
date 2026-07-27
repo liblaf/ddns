c3316a172d9317e3d3f4aa46f928be436cb1e026
liblaf <30631553+liblaf@users.noreply.github.com>
2026-07-27 11:04:47 +0800

    fix(ip): exclude reserved ranges from global addresses
    Unmap IPv4-mapped addresses and reject Tailscale and Mihomo TUN ranges.

--- pkg/ip/global.go
+++ pkg/ip/global.go
@@ -2,11 +2,21 @@ package ip
 
 import "net/netip"
 
+var reservedPrefixes = [...]netip.Prefix{
+	// ref: <https://en.wikipedia.org/wiki/List_of_reserved_IP_addresses>
+	netip.MustParsePrefix("100.64.0.0/10"), // Tailscale
+	netip.MustParsePrefix("198.18.0.0/15"), // mihomo TUN
+}
+
 func IsGlobal(ip netip.Addr) bool {
-	return ip.IsValid() &&
-		!(ip.IsInterfaceLocalMulticast() ||
-			ip.IsLinkLocalMulticast() ||
-			ip.IsLinkLocalUnicast() ||
-			ip.IsLoopback() ||
-			ip.IsPrivate())
+	ip = ip.Unmap()
+	if ip.IsPrivate() || !ip.IsGlobalUnicast() {
+		return false
+	}
+	for _, prefix := range reservedPrefixes {
+		if prefix.Contains(ip) {
+			return false
+		}
+	}
+	return true
 }
